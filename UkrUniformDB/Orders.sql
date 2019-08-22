02ae277f4fc03626fdc1507616a393e7f313eba5
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-22 23:06:21 +0300

    Added table of orders for the final purchase amount

---
+++ UkrUniformDB/Orders.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NOT NULL, 
+    [TotalPaid] DECIMAL NOT NULL, 
+    [OrderDate] DATETIME NOT NULL, 
+    [Status] INT NOT NULL, 
+    [Comment] NVARCHAR(MAX) NULL
+)
