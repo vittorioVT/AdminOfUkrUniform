9906e67be56775b9df2bee4c6ab90d55d4e9ab4a
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-22 23:18:53 +0300

    Added table OrdersDetail for description the
    key moments of order

---
+++ UkrUniformDB/OrdersDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrdersDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [OrderId] INT NOT NULL, 
+    [ProductItemId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [TotalPrice] DECIMAL NOT NULL, 
+    [Comment] NVARCHAR(MAX) NULL
+)
