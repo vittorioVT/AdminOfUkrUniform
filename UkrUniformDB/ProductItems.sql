7181a9972693d81a21451f2d8b7f14a2913f7f57
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-22 22:59:34 +0300

    Created table for products of store

---
+++ UkrUniformDB/ProductItems.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE [dbo].[ProductItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Picture] NVARCHAR(MAX) NULL, 
+    [Price] DECIMAL NOT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Name of product',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ProductItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'size, color, facture',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ProductItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Description'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'photo of product',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ProductItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Picture'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'price in dollars',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ProductItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Price'
