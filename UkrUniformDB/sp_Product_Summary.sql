d910620e30249390cb81da9585a35662dcabfe28
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-23 23:59:58 +0300

    Created stored procedure ProductSummary

---
+++ UkrUniformDB/sp_Product_Summary.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[sp_ProductSummary]
+AS
+	BEGIN
+		SELECT [ProdurctSpecifications].[ProductName]
+		       ,[ProdurctSpecifications].[Manufacturer]
+			   ,[ProdurctSpecifications].[ProductCount]
+		FROM [dbo.ProdurctSpecifications];
+	END	
+
