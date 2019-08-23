1acf399f4effa44947a84ae4716789a8020bf811
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-23 23:20:45 +0300

    Created table that contains size, gender and plastUlad

---
+++ UkrUniformDB/NatureProduction.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[NatureProduction]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Sex] NVARCHAR(50) NOT NULL, 
+    [PlastUlad] NVARCHAR(50) NOT NULL, 
+    [Size] NVARCHAR(50) NULL
+)
