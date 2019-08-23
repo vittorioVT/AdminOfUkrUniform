959b967294bce371084f2448970dcfabc9a5dfab
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-23 23:13:19 +0300

    Created table that stores product data

---
+++ UkrUniformDB/ProdurctSpecifications.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProdurctSpecifications]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductName ] NVARCHAR(50) NOT NULL, 
+    [Manufacturer] NVARCHAR(50) NOT NULL, 
+    [ProductCount] INT NOT NULL DEFAULT 0, 
+    [Price] MONEY NOT NULL 
+)
