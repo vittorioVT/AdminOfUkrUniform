ef614426f41a23e6587600a7cd251b0cb2c2f15f
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-08-23 23:38:14 +0300

    Added stored procedure for table ProductItems

---
+++ UkrUniformDB/sp_Products_Select.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sp_Products_Select]
+@Id int -- Identity Product
+AS
+BEGIN
+	SELECT[ProductItems].[Id]
+         ,[ProductItems].[Picture]
+		 ,[ProductItems].[Price]
+         ,[ProductItems].[Description]
+	 FROM [dbo].[ProductItems];
+END
